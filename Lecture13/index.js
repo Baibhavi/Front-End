cbf388faedb6714aafed630e515ad056df38cbaf
Baibhavi <baibhavipandey@gmail.com>
2023-10-09 13:59:26 +0530

    introduction to javascript

---
+++ Lecture13/index.js
@@ -0,0 +1,59 @@
+var a = 9
+var y = "hello"
+console.log(a)
+console.log(y)
+//datatypes in javascript
+// There are 6 datatypes in js :-
+//      1.Number --> 78, 78.5, -19, - 2.3
+//      2.String --> "Hello"
+//      3.Boolean --> true, false
+//      4.Object
+//      5.undefined
+//      6.Null -->empty
+
+// arrays
+var arr = [5,7,6,9,"hello",[1,2,3]]
+console.log(arr[2]);
+
+//loops
+for(var i = 0 ; i<6; i++){
+    console.log(arr[i]);
+}
+
+//to add element in last of an array we use push() function and to remove element from last use pop() function
+console.log(arr);
+arr.push(68);
+console.log(arr);
+arr.pop();
+arr.pop();
+arr.pop();
+console.log(arr);
+
+//to remove element from starting of an array we use shift() function and to add element to first use unshift() function
+arr.shift();
+console.log(arr)
+arr.unshift(56);
+console.log(arr)
+
+//to remove element from any index use splice(index no, number of element to be removed) function 
+arr.splice(2,1);
+console.log(arr)
+//splice is also used to replace elements in array splice(index no, number of element to be removed, element to be placed in place of removed elements)
+arr.splice(1,0,2,3,4,5,90)
+console.log(arr)
+
+
+// for of loop
+for(var item of arr){
+    console.log(item)
+}
+
+//length of array
+console.log(arr.length)
+
+var arr2 = [34,76,768,213,8];
+var arr3 = []
+for(var i = 0; i < arr2.length; i++){
+    arr3[i] = arr2[i]*2
+}
+console.log(arr3)
