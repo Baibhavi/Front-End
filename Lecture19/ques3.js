469e2077c07c7f9cf8c8d51a9e1af6ba6c27fdd8
Baibhavi <baibhavipandey@gmail.com>
2023-12-05 19:49:11 +0530

    DOM

---
+++ Lecture19/ques3.js
@@ -0,0 +1,38 @@
+// promise question
+
+let pr = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        let data = [
+            {name:'laptop', price:200},
+            {name:'mobile', price:100},
+            {name:'tablet', price:120},
+            {name:'pen', price:10},
+        ]
+        resolve(data);
+    }, 4000)
+});
+
+pr
+    .then((x)=>{
+     // console.log(x);
+        let arr = x.filter((item) =>{
+            if(item.price>=100){
+                return true;
+            }
+        });
+        return(arr);
+    })
+
+    .then((arr)=>{
+        let x = arr.map((item)=>{
+            return item.name;
+        })
+        return x;
+    })
+
+    .then((z)=>{
+        console.log(z);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
